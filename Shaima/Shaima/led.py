15251f4bce1296fb5a081411403ad5a58e959be0
ShaimaHussien <100887257+ShaimaHussien@users.noreply.github.com>
2022-03-03 19:28:52 -0600

    Add files via upload

---
+++ Shaima/Shaima/led.py
@@ -0,0 +1,14 @@
+#blink.py
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(25, GPIO.IN)
+while True:
+    if GPIO.input(25):
+        GPIO.output(18,False)
+    else:
+        GPIO.output(18, True)
+    
